cd0d695b164ab492da56e80de1347f34259cda0c
Dave Briccetti <daveb@davebsoft.com>
2020-01-01 21:53:11 -0800

    Add buttons to change desired temperature. Simplify HTML and styling.

--- src/static/client.js
+++ src/static/client.js
@@ -1,5 +1,6 @@
 class Client {
-  constructor() {
+  constructor(starting_desired_temp) {
+    this.desired_temp = starting_desired_temp;
     fetch('all-status').then(r => r.json()).then(j => addAllStateRecords(j));
     new EventSource('/status').onmessage = event => this.processEvent(JSON.parse(event.data));
   }
@@ -7,18 +8,19 @@ class Client {
   processEvent(state) {
     addStateRecord(state);
     const el = id => document.getElementById(id);
-    el('outside-temperature').textContent = state.outside_temp.toFixed(1);
+    el('outside-temperature').textContent = state.outside_temp.toFixed(0);
     el('temperature').textContent = state.current_temp.toFixed(1);
-    el('humidity').textContent = state.current_humidity.toFixed(1);
-    el('display-desired').textContent = el('desired').value = state.desired_temp.toFixed(1);
+    el('humidity').textContent = state.current_humidity.toFixed(0);
+    el('display-desired').textContent = state.desired_temp.toFixed(1);
   }
 
-  setDesired(desired) {
+  adjustTemp(amount) {
+    this.desired_temp += amount;
     fetch('desired', {
       method: 'PUT',
-      body: desired
+      body: this.desired_temp
     }).then(response => response);
   }
 }
 
-const client = new Client();
+const client = new Client(21.0);
