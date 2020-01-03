d1856e38f1bfb365b3f7445a44f96772f98a2dab
Dave Briccetti <daveb@davebsoft.com>
2020-01-03 13:52:52 -0800

    Tighten the reasonableness for sensor values

--- src/rpi/sensor.py
+++ src/rpi/sensor.py
@@ -3,6 +3,7 @@ import Adafruit_DHT
 from sensorfail import SensorReadFailure
 
 REASONABLE_TEMP_RANGE = 10, 50
+REASONABLE_HUMIDITY_RANGE = 20, 100
 
 
 class Sensor:
@@ -13,9 +14,10 @@ class Sensor:
 
     def read(self):
         h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, self.pin)
-        if not self._reasonable_sersor_values(h, t):
+        if not self._reasonable_sensor_values(h, t):
             raise SensorReadFailure(h, t)
         return h, t
 
-    def _reasonable_sersor_values(self, h, t):
-        return h and t and 0 < h < 100 and REASONABLE_TEMP_RANGE[0] < t < REASONABLE_TEMP_RANGE[1]
+    def _reasonable_sensor_values(self, h, t):
+        return h and t and REASONABLE_HUMIDITY_RANGE[0] < h < REASONABLE_HUMIDITY_RANGE[1] and \
+               REASONABLE_TEMP_RANGE[0] < t < REASONABLE_TEMP_RANGE[1]
