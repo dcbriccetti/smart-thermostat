786f0ca357247be3eca1cc25775d94803a2d7413
Dave Briccetti <daveb@davebsoft.com>
2020-01-04 12:40:00 -0800

    Don’t require Python 3.7 for date parsing

--- src/schedule.py
+++ src/schedule.py
@@ -3,13 +3,12 @@ from datetime import datetime
 
 class Schedule:
     def __init__(self):
-        self._settings = [(datetime.fromisoformat(s[0]), s[1]) for s in (
-            ('2020-01-03 22:45', 17.0),
-            ('2020-01-04 05:45', 22.0),
+        self._settings = [(datetime.strptime(s[0], '%Y-%m-%d %H:%M'), s[1]) for s in (
+            ('2020-01-04 12:41', 25.0),
+            ('2020-01-04 12:42', 22.0),
         )]
 
     def get_setting(self):
         if self._settings and datetime.now() > self._settings[0][0]:
             return self._settings.pop(0)[1]
         return None
-
