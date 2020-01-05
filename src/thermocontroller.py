1dab9b7f8e56c01747087d95eeb4dad0e4aaef7f
Dave Briccetti <daveb@davebsoft.com>
2020-01-05 15:22:05 -0800

    Fetch METAR every 5 minutes

--- src/thermocontroller.py
+++ src/thermocontroller.py
@@ -6,7 +6,7 @@ from sensorfail import SensorReadFailure
 from metardecoder import outside_weather
 
 TEMP_CHECK_INTERVAL_SECS = 30
-OUTSIDE_WEATHER_CHECK_INTERVAL_SECS = 60 * 15
+OUTSIDE_WEATHER_CHECK_INTERVAL_SECS = 60 * 5
 HEAT_PSEUDO_TEMP = 23
 
 
