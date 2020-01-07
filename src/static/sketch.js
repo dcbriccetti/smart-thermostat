b200dc3332f517cae058a99ccd4a8e657500d872
Dave Briccetti <daveb@davebsoft.com>
2020-01-07 14:30:15 -0800

    Improve drawing performance

--- src/static/sketch.js
+++ src/static/sketch.js
@@ -5,49 +5,6 @@ function setup() {
   createCanvas(vis.width(), vis.height()).parent('visualization');
 }
 
-function drawGridLines(temp_min, temp_max, scy) {
-  const gridLow = floor(temp_min);
-  const gridHigh = ceil(temp_max);
-  const smallRange = gridHigh - gridLow < 5;
-
-  for (let gt = gridLow; gt < gridHigh; gt++) {
-    const gly = scy(gt);
-    strokeWeight(1);
-    stroke(200);
-    line(0, gly, width, gly);
-
-    if (smallRange || gt % 5 === 0) {
-      push();
-      scale(1, -1);
-      strokeWeight(1);
-      textAlign(RIGHT, CENTER);
-      textStyle(NORMAL);
-      text(gt, width - 3, -gly);
-      pop();
-    }
-  }
-}
-
-function drawXGridLine(timeStart, x, chartYBase) {
-  const is15MinMultiple = timeStart % (60 * 15) === 0;
-  const is60MinMultiple = is15MinMultiple && timeStart % (60 * 60) === 0;
-
-  if (is15MinMultiple) {
-    strokeWeight(is60MinMultiple ? 2 : 1);
-    stroke(128);
-    line(x, chartYBase, x, height);
-  }
-
-  if (is60MinMultiple) {
-    push();
-    scale(1, -1);
-    textAlign(CENTER, BOTTOM);
-    strokeWeight(0);
-    text(new Date(timeStart * 1000).getHours(), x, 0);
-    pop();
-  }
-}
-
 function draw() {
   frameRate(0.5);
   background('#e0e0e0');
@@ -65,10 +22,10 @@ function draw() {
   const temp_max = minOrMax(Math.max, -50);
   const chartYBase = 20;
 
+  let timeStart = stateRecords[0].time;
   let timeEnd = int(Date.now() / 1000);
 
   let xRight = width - 20;
-  let iState = stateRecords.length - 1;
 
   function tempAge(rec) {
     return (rec.time - rec.outside_temp_collection_time) / 60;
@@ -76,20 +33,65 @@ function draw() {
 
   console.time('drawpoints');
   const tempToY = y => map(y, temp_min, temp_max, chartYBase, height);
-  drawGridLines(temp_min, temp_max, tempToY);
-  for (let i = stateRecords.length - 1; i >= 0; --i) {
+  const timeToX = time => map(time, timeStart, timeEnd, 0, xRight);
 
+  function drawVertGridLines() {
+    const gridLow = floor(temp_min);
+    const gridHigh = ceil(temp_max);
+    const smallRange = gridHigh - gridLow < 5;
+
+    for (let gt = gridLow; gt < gridHigh; gt++) {
+      const gly = tempToY(gt);
+      strokeWeight(1);
+      stroke(200);
+      line(0, gly, width, gly);
+
+      if (smallRange || gt % 5 === 0) {
+        push();
+        scale(1, -1);
+        strokeWeight(1);
+        textAlign(RIGHT, CENTER);
+        textStyle(NORMAL);
+        text(gt, width - 3, -gly);
+        pop();
+      }
+    }
+  }
+
+  function drawHorzGridLines() {
+    const fifteenMins = 60 * 15;
+    const sixtyMins = 60 * 60;
+    const firstLineTime = timeStart - timeStart % fifteenMins + fifteenMins;
+
+    for (let lineTime = firstLineTime; lineTime < timeEnd; lineTime += fifteenMins) {
+      const lineX = timeToX(lineTime);
+      const is60MinMultiple = lineTime % sixtyMins === 0;
+      strokeWeight(is60MinMultiple ? 2 : 1);
+      stroke(128);
+      line(lineX, chartYBase, lineX, height);
+      if (is60MinMultiple) {
+        push();
+        scale(1, -1);
+        textAlign(CENTER, BOTTOM);
+        strokeWeight(0);
+        text(new Date(lineTime * 1000).getHours(), lineX, 0);
+        pop();
+      }
+    }
+  }
+
+  drawVertGridLines();
+  drawHorzGridLines();
+
+  for (let i = stateRecords.length - 1; i >= 0; --i) {
     const rec = stateRecords[i];
-    let timeStart = timeEnd - client.sliceSecs;
-    const timeToX = time => map(time, stateRecords[0].time, timeEnd, 0, xRight);
 
     const cty = tempToY(rec.current_temp);
     const dty = tempToY(rec.desired_temp);
+
     const oat = tempToY(rec.outside_temp);
 
     const x = timeToX(rec.time);
-
-//      drawXGridLine(timeStart, x, chartYBase);
 
     strokeWeight(3);
     stroke('blue');
