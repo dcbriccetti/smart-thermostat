b5337d1623237ecb390a3e5e8ef06ec062d502fb
Dave Briccetti <daveb@davebsoft.com>
2021-02-06 11:47:12 -0800

    Improve auto scaling

--- ts/sketch.ts
+++ ts/sketch.ts
@@ -18,7 +18,29 @@ const thermoSketch = new p5(p => {
 
     if (stateRecords.length === 0) return
 
-    const minOrMax = (reduce_fn, initial_value) => stateRecords.reduce(reduce_fn, initial_value)
+    let timeStart = stateRecords[0].time
+    let timeEnd = p.int(Date.now() / 1000)
+
+    let xRight = p.width - 20
+
+    function timeToX(time) {
+      const secondsFromEnd = timeEnd - time
+      const pixelsFromEnd = secondsFromEnd / thermoClient.sliceSecs
+      return xRight - pixelsFromEnd
+    }
+
+    // Find leftmost visible record, so we can draw from left to right
+    let leftmost_visible_record_index = 0
+    for (let i = stateRecords.length - 1; i >= 0; --i) {
+      if (timeToX(stateRecords[i].time) < 0) {
+        leftmost_visible_record_index = i
+        break
+      }
+    }
+
+    const visibleStateRecords = stateRecords.slice(leftmost_visible_record_index)
+
+    const minOrMax = (reduce_fn, initial_value) => visibleStateRecords.reduce(reduce_fn, initial_value)
     const createTempReduceFn = minMaxFn => (a, c) => minMaxFn(a, c.inside_temp, c.desired_temp, c.outside_temp)
     const createPressureReduceFn = minMaxFn => (a, c) => minMaxFn(a, c.pressure)
     const y_axis_margin_degrees = 1
@@ -29,19 +51,8 @@ const thermoSketch = new p5(p => {
     const pressure_max = minOrMax(createPressureReduceFn(Math.max), 0) + y_axis_margin_hPa
     const chartYBase = 20
 
-    let timeStart = stateRecords[0].time
-    let timeEnd = p.int(Date.now() / 1000)
-
-    let xRight = p.width - 20
-
     const tempToY = temp => p.map(temp, temp_min, temp_max, chartYBase, p.height)
     const pressureToY = pressure => p.map(pressure, pressure_min, pressure_max, chartYBase, p.height)
-
-    function timeToX(time) {
-      const secondsFromEnd = timeEnd - time
-      const pixelsFromEnd = secondsFromEnd / thermoClient.sliceSecs
-      return xRight - pixelsFromEnd
-    }
 
     function drawVertGridLines() {
       const gridLow = Math.floor(temp_min)
@@ -91,16 +102,7 @@ const thermoSketch = new p5(p => {
     drawVertGridLines()
     drawHorzGridLines()
 
-    // Find leftmost visible record, so we can draw from left to right
-    let leftmost_visible_record_index = 0
-    for (let i = stateRecords.length - 1; i >= 0; --i) {
-      if (timeToX(stateRecords[i].time) < 0) {
-        leftmost_visible_record_index = i
-        break
-      }
-    }
-
-    for (const rec of stateRecords.slice(leftmost_visible_record_index)) {
+    for (const rec of visibleStateRecords) {
       const x = timeToX(rec.time)
 
       p.strokeWeight(3)
@@ -110,8 +112,10 @@ const thermoSketch = new p5(p => {
         p.point(x, tempToY(rec.desired_temp))
       }
 
-      p.stroke('blue')
-      p.point(x, pressureToY(rec.pressure))
+      if (false) { // Pressure doesn’t belong on a Celsius scale
+        p.stroke('blue')
+        p.point(x, pressureToY(rec.pressure))
+      }
 
       if (thermoClient.showingOutsideTemp) {
         p.stroke(255, 190, 0)
