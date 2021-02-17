214da4d5545cd26d2b847c05b54d5bcc0664f577
Dave Briccetti <daveb@davebsoft.com>
2021-02-16 17:03:05 -0800

    Improve scheduler code

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, List, Iterable, Tuple, Any
 from datetime import datetime, timedelta
 from thermocontroller import ThermoController
 
@@ -7,11 +7,27 @@ class Setting(NamedTuple):
     datetime: datetime
     temperature: float
 
+    @classmethod
+    def from_string(cls, string: str):
+        parts = string.split()
+        if len(parts) == 2:
+            now = datetime.now()
+            when, temp = parts
+            hm = [int(n) for n in when.split(':')]
+            hour: int = hm[0]
+            minute: int = hm[1] if len(hm) == 2 else 0
+            temperature = float(temp)
+            next_setting_time = datetime(now.year, now.month, now.day, hour, minute)
+            if next_setting_time < now:  # That time today has passed
+                next_setting_time += timedelta(1)  # Advance to tomorrow
+            return cls(next_setting_time, temperature)
+
+        raise ValueError(f'{string} does not match hh[:mm] t')
 
 class Scheduler:
-    def __init__(self, controller: ThermoController):
-        self.controller = controller
-        self._settings: list[Setting] = []
+    def __init__(self, thermoController: ThermoController):
+        self.thermoController = thermoController
+        self._settings: List[Setting] = []
 
     def update(self):
         for i, setting in enumerate(self._settings):
@@ -19,22 +35,17 @@ class Scheduler:
                 advanced = Setting(setting.datetime + timedelta(1), setting.temperature)
                 self._settings[i] = advanced
                 print('Making scheduled setting to', setting)
-                self.controller.set_temperature(setting.temperature)
+                self.thermoController.set_temperature(setting.temperature)
                 print('settings now', self._settings)
 
     def set(self, text: bytes):
-        '* 23:00 21.0'
-        now = datetime.now()
+        '23:00 21.0'
         self._settings.clear()
         for line in text.decode('utf-8').splitlines():
-            parts = line.split()
-            assert parts[0] == '*'
-            hm = [int(n) for n in parts[1].split(':')]
-            temperature = float(parts[2])
-            next_setting_time = datetime(now.year, now.month, now.day, hm[0], hm[1])
-            if next_setting_time < now:  # That time today has passed
-                next_setting_time += timedelta(1)  # Advance to tomorrow
-            self._settings.append(Setting(next_setting_time, temperature))
+            try:
+                self._settings.append(Setting.from_string(line))
+            except ValueError as e:
+                print(e)
 
         print(self._settings)
 
